0c0a38c5372318958ef5f6c64ed6b07304d88e7b
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-09-19 13:15:04 +0200

    Added Header section & css style

--- rimty-portfolio/src/App.js
+++ rimty-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// import Header from "./components/Header/Header";
+ import Header from "./components/Header/Header";
 // import Home from "./components/Home/Home";
 // import Footer from "./components/Footer/Footer";
 // import About from "./components/About/About";
@@ -11,8 +11,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <Home />
+    <Header />
+        {/* <Home />
       <Qualification />
       <Portfolio />
       <Skills />
