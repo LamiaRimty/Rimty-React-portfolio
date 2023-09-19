bbd9ec6b0597d576d0fe3e097bf637f7dbf34b61
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-09-19 13:04:35 +0200

    ReactJS Environment Setup

---
+++ rimty-portfolio/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+// import Header from "./components/Header/Header";
+// import Home from "./components/Home/Home";
+// import Footer from "./components/Footer/Footer";
+// import About from "./components/About/About";
+// import Skills from "./components/Skills/Skills";
+// import Qualification from "./components/Qualification/Qualification";
+// import Portfolio from "./components/Portfolio/Portfolio";
+// import Contact from "./components/Contact/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header />
+      <Home />
+      <Qualification />
+      <Portfolio />
+      <Skills />
+      <About />
+      <Contact />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
