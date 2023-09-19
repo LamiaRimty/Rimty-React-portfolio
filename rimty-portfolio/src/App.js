dc927fd7b4b46deb73cfc1497facddf856c5d4b4
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-09-19 13:31:53 +0200

    Added Home section & css style

--- rimty-portfolio/src/App.js
+++ rimty-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
  import Header from "./components/Header/Header";
-// import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
+ import Home from "./components/Home/Home";
+ import Footer from "./components/Footer/Footer";
 // import About from "./components/About/About";
 // import Skills from "./components/Skills/Skills";
 // import Qualification from "./components/Qualification/Qualification";
@@ -12,7 +12,8 @@ function App() {
   return (
     <div className="App">
     <Header />
-        {/* <Home />
+       <Home />
+         {/*
       <Qualification />
       <Portfolio />
       <Skills />
